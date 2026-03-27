2acab46bdd69a10eb150157d863cc70fcd9bacff
Ekaterina <keti10-96@mail.ru>
2026-03-27 12:40:14 +0300

    добавлен проект для нагрузочного тестирования

---
+++ LoadTests/Program.cs
@@ -0,0 +1,39 @@
+﻿using NBomber.Contracts;
+using NBomber.CSharp;
+using System.Text;
+
+namespace LoadTests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //в задании было указано Step.Create, но у него нет такого метода.
+            var scenario = Scenario.Create("my client-server scenario", async (context) =>
+            {
+                var step = Step.Run("set", context, async () =>
+                {
+                    using var client = new SimpleTcpClient("127.0.0.1", 8080);
+                    try
+                    {
+                        await client.ConnectAsync();
+                        await client.SetAsync("hello", Encoding.UTF8.GetBytes("123"));
+                        return Response.Ok();
+                    }
+                    catch
+                    {
+                        return Response.Fail();
+                    }
+                });
+
+                return Response.Ok();
+            })
+                .WithWarmUpDuration(TimeSpan.FromSeconds(10))
+                .WithLoadSimulations(Simulation.Inject(100, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(30)));
+
+            NBomberRunner
+            .RegisterScenarios(scenario)
+            .Run();
+        }
+    }
+}
---
+++ LoadTests/SimpleTcpClient.cs
@@ -0,0 +1,47 @@
+﻿using System.Buffers;
+using System.Net.Sockets;
+using System.Text;
+
+namespace LoadTests
+{
+    internal class SimpleTcpClient: IDisposable
+    {
+        ArrayPool<byte> pool = ArrayPool<byte>.Shared;
+        private readonly string _host;
+        private readonly int _port;
+        private Socket _socket;
+
+        public SimpleTcpClient(string host, int port)
+        {
+            _host = host;
+            _port = port;
+        }
+
+        public async Task ConnectAsync()
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);           
+            await _socket.ConnectAsync(_host, _port);  
+        }
+
+        public async Task SetAsync(string key, byte[] value)
+        {
+            var data = Encoding.UTF8.GetBytes($"SET {key} ").Concat(value).ToArray();
+            await _socket.SendAsync(data);
+        }
+
+        public async Task GetAsync(string key)
+        {
+            var buffer = pool.Rent(1096);
+            try
+            {
+                await _socket.ReceiveAsync(buffer);
+            }
+            finally
+            {
+                pool.Return(buffer, true);
+            }
+        }
+
+        public void Dispose() => _socket.Dispose();
+    }
+}
